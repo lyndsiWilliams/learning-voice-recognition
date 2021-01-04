9f07938ebfcbeab99f42f43cbddf98246aac8f9d
lyndsiWilliams <kcatgirl@gmail.com>
2021-01-04 16:07:35 -0600

    Speech being recognized in console!

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
 function App() {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+
+  recognition.onstart = () => {
+    console.log("Voice is activated, the browser is listening!");
+  };
+
+  recognition.onresult = event => {
+    console.log(event);
+  }
+
+  const handleClick = () => {
+    recognition.start();
+  };
+
   return (
     <div className="App">
-      <button className="talk">Talk</button>
+      <button className="talk" onClick={handleClick}>Talk</button>
       <h3 className="content"></h3>
     </div>
   );
